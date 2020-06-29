0ec208265770e752803c48f75129408309482387
pengjingbang <752679464@qq.com>
2020-06-29 19:58:02 +0800

    添加本地仓库？？？？

--- hellothymeleaf/src/main/java/controller/contro/contro/controller.java
+++ hellothymeleaf/src/main/java/controller/contro/contro/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class controller {
 @RequestMapping("/hrhr")//访问http://localhost:6969/hrhr
     public String main(){
-        System.out.println("控制器.。。。。。");
+        System.out.println("控制器.99999999999");
         return "studyhtml" ;
     }
 }
